a119a882701b4054384aeb2693c161ba93c2dd98
Jan Czizikow <jan.czizikow@gmail.com>
2018-08-09 16:01:48 +0200

    Generating DosesController + adding inital controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'cocktails#index'
-  resources :cocktails, only: %i[index show new create]
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[create destroy]
+  end
 end
