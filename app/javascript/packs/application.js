8a11ed84790b4814a4e3908d8e0fe859cbb80031
Jan Czizikow <jan.czizikow@gmail.com>
2018-08-12 02:39:03 +0200

    Image optimizations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,14 @@
 import 'bootstrap';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
+import 'lazysizes';
+
 import { scrollToCocktails } from '../components/hero';
 import { scrollToTop } from '../components/footer';
 // import { initUpdateNavbarOnScroll } from '../components/nav';
-// initUpdateNavbarOnScroll();
-scrollToCocktails();
-scrollToTop();
+
+document.addEventListener('DOMContentLoaded', e => {
+  // initUpdateNavbarOnScroll();
+  scrollToCocktails();
+  scrollToTop();
+});
